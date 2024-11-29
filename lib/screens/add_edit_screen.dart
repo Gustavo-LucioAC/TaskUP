e27a3b5969ed0488724bac0de18fc434dd8355d3
Gustavo L <gtsug5@gmail.com>
2024-11-28 23:30:25 -0300

    app completo

---
+++ lib/screens/add_edit_screen.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:taskup/model/notes_model.dart';
+import 'package:taskup/screens/home_screen.dart';
+import 'package:taskup/service/database_helper.dart';
+
+class AddEditNoteScreen extends StatefulWidget {
+  final Note? note;
+  const AddEditNoteScreen({this.note});
+
+  @override
+  State<AddEditNoteScreen> createState() => _AddEditNoteScreenState();
+}
+
+class _AddEditNoteScreenState extends State<AddEditNoteScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _titleController = TextEditingController();
+  final _contentController = TextEditingController();
+  final DatabaseHelper _databaseHelper = DatabaseHelper();
+  Color _selectedColor = Colors.amber;
+  final List<Color> _colors = [
+    Colors.amber,
+    Color(0xFF50C878),
+    Colors.redAccent,
+    Colors.blueAccent,
+    Colors.indigo,
+    Colors.purpleAccent,
+    Colors.pinkAccent,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.note != null) {
+      _titleController.text = widget.note!.title;
+      _contentController.text = widget.note!.content;
+      _selectedColor = Color(int.parse(widget.note!.color));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(widget.note == null ? 'Add Note' : "Edit Note"),
+      ), //appBar
+      body: Form(
+        key: _formKey,
+        child: Column(children: [
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(children: [
+              TextFormField(
+                controller: _titleController,
+                decoration: InputDecoration(
+                  hintText: "Title",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ), //OUTLINEINPUTBORDE
+                ), // INPUTDECORATION
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter a tittle";
+                  }
+                  return null;
+                },
+              ), // TextFoRmField
+              SizedBox(height: 16),
+              TextFormField(
+                controller: _contentController,
+                decoration: InputDecoration(
+                  hintText: "Content",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ), //OUTLINEINPUTBORDE
+                ), // INPUTDECORATION
+                maxLines: 10,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter a Content";
+                  }
+                  return null;
+                },
+              ), // TextFoRmField
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: _colors.map(
+                      (color) {
+                        return GestureDetector(
+                          onTap: () => setState(() => _selectedColor = color),
+                          child: Container(
+                            height: 40,
+                            width: 40,
+                            margin: EdgeInsets.only(right: 8),
+                            decoration: BoxDecoration(
+                              color: color,
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: _selectedColor == color
+                                    ? Colors.black45
+                                    : Colors.transparent,
+                                width: 2,
+                              ), //Borderall
+                            ), //boxDecoration
+                          ), //Container
+                        ); // GestureDetector
+                      },
+                    ).toList(),
+                  ), //Row
+                ), //SingleChildScrollVieww
+              ), //padding
+              InkWell(
+                onTap: () {
+                  _saveNote();
+                  Navigator.pop(context);
+                },
+                child: Container(
+                    margin: EdgeInsets.all(20),
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                        color: Color(0xFF50C878),
+                        borderRadius:
+                            BorderRadius.circular(10)), //BoxDecoration
+                    child: Center(
+                      child: Text(
+                        "Save Note",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ), //textStyle
+                      ), // Text
+                    ) //center
+                    ), //Container
+              ), //Inwell
+            ]), //column
+          ), //Padding
+        ]), //column
+      ), //Form
+    ); //Scaffold
+  }
+
+  Future<void> _saveNote() async {
+    if (_formKey.currentState!.validate()) {
+      final note = Note(
+        id: widget.note?.id,
+        title: _titleController.text,
+        content: _contentController.text,
+        color: _selectedColor.value.toString(),
+        dateTime: DateTime.now().toString(),
+      ); //note
+      if (widget.note == null) {
+        await _databaseHelper.insertNote(note);
+      } else {
+        await _databaseHelper.updateNote(note);
+      }
+    }
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:taskup/model/notes_model.dart';
+import 'package:taskup/screens/add_edit_screen.dart';
+import 'package:taskup/screens/view_note_screen.dart';
 import 'package:taskup/service/database_helper.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -24,7 +26,122 @@ class _HomeScreenState extends State<HomeScreen> {
   ];
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadNotes();
+  }
+
+  Future<void> _loadNotes() async {
+    final notes = await _databaseHelper.getNotes();
+    setState(() {
+      _notes = notes;
+    });
+  }
+
+  String _formatDateTime(String dateTime) {
+    final DateTime dt = DateTime.parse(dateTime);
+    final now = DateTime.now();
+
+    if (dt.year == now.year && dt.month == now.month && dt.day == now.day) {
+      return 'Today, ${dt.hour.toString().padLeft(2, '0')}:${dt.minute.toString().padLeft(0, '0')}';
+    }
+
+    return '${dt.day}/${dt.month}/${dt.year}, ${dt.hour.toString().padLeft(2, '0')}:${dt.hour.toString().padLeft(0, '0')}';
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(
+          "My notes",
+        ),
+      ),
+      body: GridView.builder(
+          padding: EdgeInsets.all(16),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, crossAxisSpacing: 16, mainAxisSpacing: 16),
+          itemCount: _notes.length,
+          itemBuilder: (context, index) {
+            final note = _notes[index];
+            final color = Color(int.parse(note.color));
+
+            return GestureDetector(
+              onTap: () async {
+                await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ViewNoteScreen(note: note),
+                    ));
+                _loadNotes();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: color,
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      blurRadius: 4,
+                      offset: const Offset(0, 2),
+                    )
+                  ],
+                ),
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      note.title,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      note.content,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        color: Colors.white70,
+                      ),
+                      maxLines: 4,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Spacer(),
+                    Text(
+                      _formatDateTime(note.dateTime),
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AddEditNoteScreen(),
+              ));
+          _loadNotes();
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Color(0xFF50c878),
+        foregroundColor: Colors.white,
+      ),
+    );
   }
 }
